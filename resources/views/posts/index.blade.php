030fe5a0f82195bf7d6e1902ba3e00909857696d
kikuchiren <g031s051@s.iwate-pu.ac.jp>
2023-11-29 23:31:53 +0900

    add:ブログ投稿画面の追加

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,11 +4,13 @@
         <meta charset="utf-8">
         <title>Blog</title>
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="/css/style.css" rel="stylesheet">
     </head>
     <body>
         <h1>Blog Name</h1>
+        <a href='/posts/create'>create</a>
         <div class='posts'>
+            <div class="div-css">aaa</div>
             @foreach ($posts as $post)
                 <div class='post'>
                     <h2 class='title'>{{ $post->title }}</h2>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Posts</title>
         <!--Fonts-->
-        <linl href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet"></linl>
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">  
     </head>
     <body>
         <h1 class="title">
