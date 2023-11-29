ebda1b66d2d9a69bae905e14c9d733b8af19f559
kikuchiren <g031s051@s.iwate-pu.ac.jp>
2023-11-30 00:37:18 +0900

    add:バリデーション

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -13,11 +13,14 @@
             @csrf
             <div class="title">
                 <h2>Title</h2>
-                <input type="text" name="post[title]" placeholder="タイトル"/>
+                <input type="text" name="post[title]" placeholder="タイトル" value="{{ old('post.title') }}"/>
+                <p class="title_error" style="color:red">{{ $errors->first('post.title') }}</p>
             </div>
             <div class="body">
                 <h2>Body</h2>
-                <textarea name="post[body]" placeholder="何か言いたいことはあるか？"></textarea>
+                <textarea name="post[body]" placeholder="何か言いたいことはあるか？">{{ old('post.body') }}</textarea>
+                <p class="title_error" style="color:red">{{ $errors->first('post.body') }}</p>
+
             </div>
             <input type="submit" value="store" />
         </form>
