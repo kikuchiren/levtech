153b4203b91d1a6ec05d5f40765ba0218e17154f
kikuchiren <g031s051@s.iwate-pu.ac.jp>
2023-12-01 18:18:52 +0900

    add:ブログ投稿の編集

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+        <!-- Fonts -->
+        <link href="/css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <h1 class="title">編集画面</h1>
+        <div class="content">
+            <form action="/posts/{{ $post->id }}" method="POST">
+                @csrf
+                @method('put')
+                <div class="content__title">
+                    <h2>タイトル</h2>
+                    <input type='text' name='post[title]' value="{{ $post->title }}">
+                <p class="title_error" style="color:red">{{ $errors->first('post.title') }}</p>
+                </div>
+                <div class='content__body'>
+                    <h2>本文</h2>
+                    <input type='text' name='post[body]' value="{{ $post->body }}">
+                    <p class="title_error" style="color:red">{{ $errors->first('post.body') }}</p>
+                </div>
+                <input type="submit" value="保存">
+            </form>
+        </div>
+    </body>
+</html>
